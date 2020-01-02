23d51ca166dd900091baca8632e568df918932e4
zozidalom <zozo.nagy02@gmail.com>
2020-01-02 18:38:26 +0100

    Added configuration support

--- src/main/java/com/zozidalom/cdu/Config.java
+++ src/main/java/com/zozidalom/cdu/Config.java
@@ -1,20 +1,8 @@
 package com.zozidalom.cdu;
 
-import lombok.Getter;
-
-import java.util.ArrayList;
+import java.util.HashSet;
 
 public class Config {
-  @Getter
-  private static String ipProvider;
-  @Getter
-  private static String updateInterval;
-  @Getter
-  private static String email;
-  @Getter
-  private static String domain;
-  @Getter
-  private static String globalApiKey;
-  @Getter
-  private static String zoneId;
+  public static HashSet<String> domainList;
+  public static String emailAddress, zoneId, globalApiKey;
 }
--- src/main/java/com/zozidalom/cdu/Main.java
+++ src/main/java/com/zozidalom/cdu/Main.java
@@ -1,17 +1,19 @@
 package com.zozidalom.cdu;
 
-import eu.roboflax.cloudflare.CloudflareAccess;
-import eu.roboflax.cloudflare.CloudflareRequest;
-import eu.roboflax.cloudflare.CloudflareResponse;
-import eu.roboflax.cloudflare.constants.Category;
-import eu.roboflax.cloudflare.objects.dns.DNSRecord;
+import com.google.gson.JsonParser;
+import gyurix.minilib.configfile.ConfigFile;
 import gyurix.minilib.utils.SU;
-import gyurix.minilib.utils.json.JsonAPI;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPut;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.util.EntityUtils;
 
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
 import java.util.Scanner;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -19,23 +21,40 @@ import java.util.concurrent.TimeUnit;
 
 public class Main {
   private static ScheduledExecutorService executor;
-  private static CloudflareAccess cloudflareAccess;
-  public static void main(String[] args) throws Exception{
-      SU.saveFiles("config.json");
-      JsonAPI.deserialize(String.join("\n", Files.readAllLines(Paths.get("config.json"))), Config.class);
-      cloudflareAccess = new CloudflareAccess(Config.getGlobalApiKey(), Config.getEmail());
-      executor = Executors.newScheduledThreadPool(1);
-      executor.scheduleAtFixedRate(() -> {
-        CloudflareResponse<List<DNSRecord>> response = new CloudflareRequest(Category.LIST_DNS_RECORDS, cloudflareAccess).identifiers(Config.getZoneId()).asObjectList(DNSRecord.class);
-        response.getObject().forEach(dnsRecord -> {
-          try {
-            if (dnsRecord.getName().equals(Config.getDomain()) && dnsRecord.getType().equals("A")) {
-              new CloudflareRequest(Category.UPDATE_DNS_RECORD, cloudflareAccess).identifiers(dnsRecord.getId()).identifiers(Config.getZoneId(), dnsRecord.getId()).body("content", new Scanner(new URL(Config.getIpProvider()).openStream(), "UTF-8").next()).send();
+  private static final CloseableHttpClient httpClient = HttpClients.createDefault();
+  private static final JsonParser parser = new JsonParser();
+  private static ConfigFile cfg;
+
+  public static void main(String[] args) {
+    SU.saveFiles("config.yml");
+    cfg = new ConfigFile(new File("config.yml"));
+    cfg.data.deserialize(Config.class);
+    executor = Executors.newScheduledThreadPool(1);
+    executor.scheduleAtFixedRate(() -> {
+      Config.domainList.forEach(domain -> {
+        try {
+          HttpGet getRequest = new HttpGet("https://api.cloudflare.com/client/v4/zones/" + Config.zoneId + "/dns_records?name=" + domain);
+          getRequest.addHeader("X-Auth-Email", Config.emailAddress);
+          getRequest.addHeader("X-Auth-Key", Config.globalApiKey);
+          CloseableHttpResponse response = httpClient.execute(getRequest);
+          parser.parse(EntityUtils.toString(response.getEntity())).getAsJsonObject().getAsJsonArray("result").forEach(jsonElement -> {
+            if (jsonElement.getAsJsonObject().get("type").getAsString().equals("A")) {
+              HttpPut putRequest = new HttpPut("https://api.cloudflare.com/client/v4/zones/" + Config.zoneId + "/dns_records/" + jsonElement.getAsJsonObject().get("id").getAsString());
+              putRequest.addHeader("X-Auth-Email", Config.emailAddress);
+              putRequest.addHeader("X-Auth-Key", Config.globalApiKey);
+              try {
+                String update = "{\"type\":\"A\",\"name\":\"" + jsonElement.getAsJsonObject().get("name").getAsString() + "\",\"content\":\"" + new Scanner(new URL("http://ipv4.icanhazip.com").openStream(), "UTF-8").next() + "\",\"ttl\":120,\"proxied\":false}";
+                putRequest.setEntity(new ByteArrayEntity(update.getBytes()));
+                httpClient.execute(putRequest);
+              } catch (IOException e) {
+                e.printStackTrace();
+              }
             }
-          } catch(Throwable ex){
-            ex.printStackTrace();
-          }
-        });
-      }, 0, Long.parseLong(Config.getUpdateInterval()), TimeUnit.SECONDS);
-    }
+          });
+        } catch (Throwable ex) {
+          ex.printStackTrace();
+        }
+      });
+    }, 0, 60, TimeUnit.SECONDS);
   }
+}
